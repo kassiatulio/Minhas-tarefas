c877a14265c3e67d8102a4a311f44b6da1583fba
Kassia Tulio <kassia.tulio@gmail.com>
2019-04-15 17:46:15 +0100

    Insert a trash button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,29 @@ $(document).ready(function() {
 
   let lastCheckboxId = $(".card").length
 
-  const modalAdd = $(".modal-add")
-
   const generateCheckboxId = () => {
     return ++lastCheckboxId
   }
-  
- $(".btn-add").click(function() {
-  modalAdd.modal()
- })
 
- $(".btn-save").click(function(){
-   const newTaskTitle = $(".task-title").val()
-   const newTaskDescription = $(".task-description").val()
+  const modalAdd = $(".modal-add")
+  const modalRemove = $(".modal-remove")
+
+  
+  $(".trash-icon").click(function(event) {
+    modalRemove.modal()
+    const cardRemove = event.target.parentNode
+    console.log(cardRemove)
+  })
+
+ 
+  $(".btn-add").click(function() {
+    modalAdd.modal()
+  })
+
+
+  $(".btn-save").click(function() {
+    const newTaskTitle = $(".task-title").val()
+    const newTaskDescription = $(".task-description").val()
    
   $(".container").append(`
     <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
@@ -24,17 +34,22 @@ $(document).ready(function() {
       </div></div>
       <div class="card-body">
         <p class="card-text">${newTaskDescription}</p>
+        <i class="trash-icon far fa-trash-alt"></i>
       </div>
     </div>
   `)
-    modalAdd.modal("hide")
+  modalAdd.modal("hide")
 
-    $(".task-title").val("") 
-    $(".task-description").val("") 
- })
+  $(".task-title").val("") 
+  $(".task-description").val("") 
+  })
+
+  $(".btn-remove").click(function() {
+    
+  })  
  
- //$(".check").click(function(){
-  //$(this).parent().parent().fadeTo(1000, 0.4);
+  $(".check").click(function(){
+  $(this).parent().parent().fadeTo(1000, 0.4);
 })
 
 });
